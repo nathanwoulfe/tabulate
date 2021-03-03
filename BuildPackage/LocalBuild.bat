7e16d5c2da02ec5bd55670372b49f4415b42875b
Nathan Woulfe <nathan@nathanw.com.au>
2021-03-04 06:46:06 +1000

    add azure pipelines, update build

--- BuildPackage/LocalBuild.bat
+++ BuildPackage/LocalBuild.bat
@@ -1,19 +1,19 @@
 ECHO off
 
-SET /P APPVEYOR_BUILD_NUMBER=Please enter a build number (e.g. 134):
+SET /P BUILD_NUMBER=Please enter a build number (e.g. 134):
 SET /P PACKAGE_VERISON=Please enter your package version (e.g. 1.0.5):
 SET /P UMBRACO_PACKAGE_PRERELEASE_SUFFIX=Please enter your package release suffix or leave empty (e.g. beta):
 
-SET /P APPVEYOR_REPO_TAG=If you want to simulate a GitHub tag for a release (e.g. true):
+SET /P REPO_TAG=If you want to simulate a GitHub tag for a release (e.g. true):
 
-if "%APPVEYOR_BUILD_NUMBER%" == "" (
-  SET APPVEYOR_BUILD_NUMBER=100
+if "%BUILD_NUMBER%" == "" (
+  SET BUILD_NUMBER=100
 )
 if "%PACKAGE_VERISON%" == "" (
   SET PACKAGE_VERISON=0.1.0
 )
 
-SET APPVEYOR_BUILD_VERSION=%PACKAGE_VERISON%.%APPVEYOR_BUILD_NUMBER%
+SET BUILD_VERSION=%PACKAGE_VERISON%.%BUILD_NUMBER%
 
 build.bat
 
