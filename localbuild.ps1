62e52f25a8521341653c30d8016a62a4c01c9565
Nathan Woulfe <nathan@nathanw.com.au>
2021-09-06 12:27:17 +1000

    build

--- localbuild.ps1
+++ localbuild.ps1
@@ -1,3 +1,15 @@
-rm -r -force __out
-mkdir __out
-dotnet pack -c Debug -o __out --no-restore
+  
+## clean up from previous runs
+rm -r -force nupkgs
+rm -r -force ./src/Tabulate.Umbraco/App_Plugins
+mkdir nupkgs
+
+## install backoffice dependencies
+cd ./src/Tabulate.Umbraco
+npm install
+npm run build
+cd ../../
+
+## pack individually to avoid plumber.site blowing up
+dotnet pack ./src/Tabulate.Umbraco.ValueConverters/Tabulate.Umbraco.ValueConverters.csproj -c Release -o nupkgs
+dotnet pack ./src/Tabulate.Umbraco/Tabulate.Umbraco.csproj -c Release -o nupkgs
